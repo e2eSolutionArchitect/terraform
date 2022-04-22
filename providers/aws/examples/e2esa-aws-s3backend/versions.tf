64ab5d812858e562fd92718787a0710839a9e2a9
e2e Solution Architect <62712515+awstechguide@users.noreply.github.com>
2022-04-22 09:46:02 -0400

    Create versions.tf

---
+++ providers/aws/examples/e2esa-aws-s3backend/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "~> 1.1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.5.0"
+    }
+  }
+}
+
+# provider block
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+  #alias   = "us-east-1"
+}
