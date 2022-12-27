92045f7bc495360b0520d7ab1e0cba320cdf1855
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2022-12-26 19:51:59 -0500

    updated cloudfront module

--- providers/aws/examples/e2esa-aws-static-webhosting/acm-certificate.tf
+++ providers/aws/examples/e2esa-aws-static-webhosting/acm-certificate.tf
@@ -1,9 +1,21 @@
 #------------------------------------------------------------------------------
 # ACM Certificate
 #------------------------------------------------------------------------------
+module "acm" {
+  #source = "../../modules/e2esa-module-aws-acm"
+  source             = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-acm?ref=main"
+  name        = "${local.name}-${var.suffix}"
+  domain_name = var.domain_name
+  additional_domain_names=var.cf_domain_names
+  tags        = merge({ "resourcename" = "${local.name}-${var.suffix}" }, local.tags)
+}
+
+
 # Find a certificate issued by (not imported into) ACM
 data "aws_acm_certificate" "amazon_issued" {
   domain      = var.domain_name
   types       = ["AMAZON_ISSUED"]
   most_recent = true
+  depends_on = [module.acm]
 }
+
--- providers/aws/examples/e2esa-aws-static-webhosting/cloudfront.tf
+++ providers/aws/examples/e2esa-aws-static-webhosting/cloudfront.tf
@@ -1,9 +1,10 @@
 module "cloudfront" {
-  source = "../../modules/e2esa-module-aws-cloudfront"
-  #source             = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-cloudfront?ref=main"
+  #source = "../../modules/e2esa-module-aws-cloudfront"
+  source             = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-cloudfront?ref=main"
   name             = "${local.name}-${var.suffix}"
   domain_name     = var.domain_name
   cf_log_s3_bucket = "${var.s3_bucket_name}.s3.amazonaws.com"
   cf_domain_names  = var.cf_domain_names
   tags             = merge({ "resourcename" = "${local.name}-${var.suffix}" }, local.tags)
+  depends_on = [module.acm]
 }
