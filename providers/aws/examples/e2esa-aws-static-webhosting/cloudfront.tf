121b8416b9bb324d0f60bb590967f2054dd71fcd
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2022-12-27 01:36:14 -0500

    TF updated for static webhost

--- providers/aws/examples/e2esa-aws-static-webhosting/cloudfront.tf
+++ providers/aws/examples/e2esa-aws-static-webhosting/cloudfront.tf
@@ -1,6 +1,6 @@
 module "cloudfront" {
-  #source = "../../modules/e2esa-module-aws-cloudfront"
-  source             = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-cloudfront?ref=main"
+  source = "../../modules/e2esa-module-aws-cloudfront"
+  #source             = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-cloudfront?ref=main"
   name             = "${local.name}-${var.suffix}"
   domain_name     = var.domain_name
   cf_log_s3_bucket = "${var.s3_bucket_name}.s3.amazonaws.com"
