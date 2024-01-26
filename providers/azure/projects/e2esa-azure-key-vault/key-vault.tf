56c5bf4f1bef653a4fae7d1b3184310879096329
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2024-01-26 12:58:17 -0500

    azure key vault updated

--- providers/azure/projects/e2esa-azure-key-vault/key-vault.tf
+++ providers/azure/projects/e2esa-azure-key-vault/key-vault.tf
@@ -125,3 +125,58 @@ resource "azurerm_key_vault_certificate" "certificate" {
     password = var.certificate_password
   }
 }
+
+
+resource "azurerm_key_vault_certificate" "certificate" {
+  name         = "${var.key_vault_name}-certificate"
+  key_vault_id = azurerm_key_vault.vault.id
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = true
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = true
+    }
+
+    lifetime_action {
+      action {
+        action_type = "AutoRenew"
+      }
+
+      trigger {
+        days_before_expiry = 30
+      }
+    }
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+
+    x509_certificate_properties {
+      # Server Authentication = 1.3.6.1.5.5.7.3.1
+      # Client Authentication = 1.3.6.1.5.5.7.3.2
+      extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
+
+      key_usage = [
+        "cRLSign",
+        "dataEncipherment",
+        "digitalSignature",
+        "keyAgreement",
+        "keyCertSign",
+        "keyEncipherment",
+      ]
+
+      subject_alternative_names {
+        dns_names = ["internal.contoso.com", "domain.hello.world"]
+      }
+
+      subject            = "CN=hello-world"
+      validity_in_months = 12
+    }
+  }
+}
