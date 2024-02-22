4549c1f50aa27c12a2d9946215c5e5b0c232e9cd
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2024-02-22 18:07:21 -0500

    update

--- providers/aws/projects/template-e2esa-aws/main.tf
+++ providers/aws/projects/template-e2esa-aws/main.tf
@@ -2,3 +2,12 @@ module "aws_lb" {
   source = "../../modules/e2esa-module-aws-elb"
   tags   = merge({ "resourcename" = "${local.name}-backup-plan-${var.suffix}" }, local.tags)
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
